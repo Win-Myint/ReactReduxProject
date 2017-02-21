1ea8fff50b8aacf96ebb861e1fd53c05b3c69dd8
Win <win.m179@gmail.com>
2017-02-21 11:23:13 +0000

    install ejs modules and set up front end to use ejs view engine

--- server.js
+++ server.js
@@ -2,9 +2,17 @@
 var express = require('express');
 var app 	= express();
 
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
 // Assign local host port number 
 var port 	= process.env.PORT || 3000;
 
 app.listen(port, function(){
 	console.log('listening to port ' + port);
 })
+
+// Respond to port 3000 http root request
+app.get('/', function(req, res){
+	console.log("Hi there");
+});
